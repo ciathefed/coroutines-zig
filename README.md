70bd0dc792423760c3b55cc97e1bb03063c8f30f
ciathefed <admin@ciacoin.xyz>
2025-03-09 16:40:26 -0700

    update build.zig and README.md

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Zoro
 
 A port of tsoding's [coroutines](https://github.com/tsoding/coroutines) in Zig
+
+## Example
+
+```zig
+const std = @import("std");
+const zoro = @import("zoro");
+
+fn counter(arg: ?*anyopaque) callconv(.C) void {
+    const n = @as(usize, @intCast(@intFromPtr(arg)));
+    for (0..n) |i| {
+        std.debug.print("[{}] {}\n", .{ zoro.id(), i });
+        zoro.yield();
+    }
+}
+
+pub fn main() !void {
+    zoro.init();
+
+    zoro.go(counter, @ptrFromInt(5));
+    zoro.go(counter, @ptrFromInt(10));
+
+    while (zoro.alive() > 1) {
+        zoro.yield();
+    }
+}
+```
--- build.zig
+++ build.zig
@@ -4,17 +4,10 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const coroutine_lib = b.addStaticLibrary(.{
-        .name = "coroutine",
-        .target = target,
-        .optimize = optimize,
-    });
-    coroutine_lib.addCSourceFile(.{ .file = b.path("src/coroutine.c") });
-    coroutine_lib.linkLibC();
-
     const module = b.addModule("zoro", .{
+        .root_source_file = b.path("src/zoro.zig"),
         .target = target,
         .optimize = optimize,
     });
-    module.linkLibrary(coroutine_lib);
+    module.addCSourceFile(.{ .file = b.path("src/coroutine.c") });
 }
